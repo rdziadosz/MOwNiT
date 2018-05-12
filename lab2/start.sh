6ccd53c9bd80bd6e9f3201ab254e4ca92eb1eed9
Radoslaw <radoslaw@dziadosz.eu>
2018-05-12 10:30:27 +0200

    License change xD

--- lab2/start.sh
+++ lab2/start.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# rdziadosz wrote this code. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return.
+#----------------------------------------------------------------------------
+
+
 echo "ZADANIE 2"
 tclsh lab2zad2.tcl | grep -E "TOTAL NDOF|H1:|INFO" >>zad2.out
 cat zad2.out
