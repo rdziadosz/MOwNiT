9ee84866a94041108db6871b3b9ef090440a0515
Radosław <radoslaw@dziadosz.eu>
2018-05-23 18:00:05 +0200

    2D

--- lab2/start.sh
+++ lab2/start.sh
@@ -5,11 +5,12 @@
 # whatever you want with this stuff. If we meet some day, and you think this
 # stuff is worth it, you can buy me a beer in return.
 #----------------------------------------------------------------------------
-# Zadanie A=1, B=2 oraz parametry z kartki
-readonly ZADANIE="1"
-readonly PARAM1="33"
-readonly PARAM2="0.51"
-readonly PARAM3="1337"
+# Zadanie A=0, B=2 oraz parametry z kartki
+# Zadanie 3 hp uruchomilem recznie (bo oj tak trzeba screeny) i skopiowalem tabelke do zad3hp.out (żeby wygenerowac wykresy).
+#readonly ZADANIE="0"
+#readonly PARAM1="43"
+#readonly PARAM2="1.74"
+#readonly PARAM3="-1.49"
 #----------------------------------------------------------------------------
 
 echo "ZADANIE 1"
@@ -25,12 +26,12 @@ tclsh lab2zad3.tcl | grep -E "TOTAL NDOF|H1:|INFO" > zad3.out
 #tclsh lab2zad3.tcl > zad3f.out
 cat zad3.out
 echo "ZADANIE 3 hp"
-cd ../data_1Dhp/
-(echo "10"; echo "0.001"; echo "30"; echo ${ZADANIE}; echo ${PARAM1}; echo ${PARAM2}; echo "1"; echo "2") | ../hp1d 2>/dev/null | grep -E "^\s+[[:digit:]]+\s+[[:digit:]]" > zad3hp.out
+#cd ../data_2Dhp/
+#(echo "10"; echo "0.001"; echo "30"; echo ${ZADANIE}; echo ${PARAM1}; echo ${PARAM2}; echo "1"; echo "2") | ../hp1d 2>/dev/null | grep -E "^\s+[[:digit:]]+\s+[[:digit:]]" > zad3hp.out
 #(echo "10"; echo "0.001"; echo "30"; echo ${ZADANIE}; echo ${PARAM1}; echo ${PARAM2}; echo "1"; echo "2") | ../hp1d 2>/dev/null > zad3hpf.out
 cat zad3hp.out
-mv *.out ../data_1D/
-cd ../data_1D/
+#mv *.out ../data_2D/
+#cd ../data_2D/
 
 sh wykresy.sh
 sh tabelki.sh
@@ -38,5 +39,5 @@ sh tabelki.sh
 mkdir sprawko
 mv *.png sprawko
 mv tabelki.txt sprawko
-rm *.out
-rm temp/*
+#rm *.out
+#rm temp/*
