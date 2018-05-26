1a2daa55b04f2aee8788baa923f713c3ba59fcf8
Radosław <radoslaw@dziadosz.eu>
2018-05-26 16:21:50 +0200

    2D

--- lab2/wykresy.sh
+++ lab2/wykresy.sh
@@ -5,14 +5,14 @@ cat zad1.out | grep 'H1'   | cut -c 29-38 > temp/z1h1log10
 cat zad1.out | grep 'NDOF' | cut -c 13-18 > temp/z1ndof
 cat zad1.out | grep 'NDOF' | cut -c 29-38 > temp/z1ndoflog10
 paste temp/z1ndoflog10 temp/z1h1log10 > temp/z1alllog10
-sed -n 1,6p   temp/z1alllog10 > temp/z1p1log10
-sed -n 7,12p  temp/z1alllog10 > temp/z1p2log10
-sed -n 13,18p temp/z1alllog10 > temp/z1p3log10
+sed -n 1,4p   temp/z1alllog10 > temp/z1p1log10
+sed -n 5,8p  temp/z1alllog10 > temp/z1p2log10
+sed -n 9,11p temp/z1alllog10 > temp/z1p3log10
 
 paste temp/z1ndof temp/z1h1 > temp/z1all
-sed -n 1,6p   temp/z1all > temp/z1p1
-sed -n 7,12p  temp/z1all > temp/z1p2
-sed -n 13,18p temp/z1all > temp/z1p3
+sed -n 1,4p  temp/z1all > temp/z1p1
+sed -n 5,8p  temp/z1all > temp/z1p2
+sed -n 9,11p temp/z1all > temp/z1p3
 
 cat zad2.out | grep 'H1'   | cut -c 5-18  > temp/z2h1
 cat zad2.out | grep 'H1'   | cut -c 29-38 > temp/z2h1log10
@@ -33,14 +33,14 @@ cat zad3.out | grep 'H1'   | cut -c 29-38 > temp/z3h1log10
 cat zad3.out | grep 'NDOF' | cut -c 13-18 > temp/z3ndof
 cat zad3.out | grep 'NDOF' | cut -c 29-38 > temp/z3ndoflog10
 paste temp/z3ndoflog10 temp/z3h1log10 > temp/z3alllog10
-cat  temp/z3alllog10 > temp/z3p1log10
-#sed -n 7,12p  temp/z3alllog10 > temp/z3p2log10
-#sed -n 13,18p temp/z3alllog10 > temp/z3p3log10
+sed -n 1,6p   temp/z3alllog10 > temp/z3p1log10
+sed -n 7,12p  temp/z3alllog10 > temp/z3p2log10
+sed -n 13,18p temp/z3alllog10 > temp/z3p3log10
 
 paste temp/z3ndof temp/z3h1 > temp/z3all
-cat   temp/z3all > temp/z3p1
-#sed -n 7,12p  temp/z3all > temp/z3p2
-#sed -n 13,18p temp/z3all > temp/z3p3
+sed -n 1,6p   temp/z3all > temp/z3p1
+sed -n 7,12p  temp/z3all > temp/z3p2
+sed -n 13,18p temp/z3all > temp/z3p3
 
 cat zad3hp.out > temp/z3hp
 
